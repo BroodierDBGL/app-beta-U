e3cdf359af078eb695f0de10550dc2231e43e492
BroodierDBGL <nicolas19denim@gmail.com>
2026-03-06 12:02:42 -0500

    creé la tabla

---
+++ config/database_backup_1.sql
@@ -0,0 +1,11 @@
+create table user{
+    /*
+    id char(10) serial primary key not null,
+    firstname varchar(50) not null,
+    lastname varchar(50) not null,
+    email varchar(100) not null constraint unique,
+    psswd text not null,
+    mobile_phone varchar(20) not null constraint unique,
+    address varchar(100) null,
+    */
+}
