adadfe7b8de59763c04969693e7e13d13da8e244
BroodierDBGL <nicolas19denim@gmail.com>
2026-03-18 08:36:07 -0500

    conectamos el proyecto  con SUPABASE

--- config/database.php
+++ config/database.php
@@ -7,14 +7,14 @@
     $LOCAL_PORT     = '5432';
 
     //Supabase Database configuration
-    $SUPA_HOST      = '';
-    $SUPA_DBNAME    = '';
-    $SUPA_USERNAME  = '';
-    $SUPA_PASSWORD  = '';
-    $SUPA_PORT      = '';
+    $SUPA_HOST      = 'aws-1-us-east-1.pooler.supabase.com';
+    $SUPA_DBNAME    = 'postgres';
+    $SUPA_USERNAME  = 'postgres.mwcttsyqjyjoaqsbmuzu';
+    $SUPA_PASSWORD  = '*Pinos0906*';
+    $SUPA_PORT      = '6543';
 
     //CREDENCIALES DE LA CONEXION
-    $data_connection  = "
+    $local_data_connection  = "
         host = $LOCAL_HOST
         dbname = $LOCAL_DBNAME
         user = $LOCAL_USERNAME
@@ -22,12 +22,31 @@
         port = $LOCAL_PORT
     ";
 
-    $conn = pg_connect($data_connection);
+    $supa_data_connection  = "
+        host = $SUPA_HOST
+        dbname = $SUPA_DBNAME
+        user = $SUPA_USERNAME
+        password = $SUPA_PASSWORD
+        port = $SUPA_PORT
+    ";
 
-    if(!$conn){
-        echo "Error: Unable to connect to database";
+    //LOCAL CONNECTION
+    $local_conn = pg_connect($local_data_connection);
+
+    if(!$local_conn){
+        echo "Error: Unable to connect to local database";
         exit();
     }else{
-        echo "Succes connection !!!";
+        echo "Local Succes connection !!!";
+    }
+
+    //SUPA CONNECTION
+    $supa_conn = pg_connect($supa_data_connection);
+
+    if(!$supa_conn){
+        echo "Error: Unable to connect to Supabase database";
+        exit();
+    }else{
+        echo "<br>Supabase Succes connection !!!";
     }
 ?>
